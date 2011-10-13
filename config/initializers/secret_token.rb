8712f927964f6d6e76a93dcc5f7ebeffee8c7067
ZIya <ziya@My-World.(none)>
2011-10-13 13:42:28 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MallLogin::Application.config.secret_token = '43b970a423e2f8b52e2699802edff638d40f676670baf46d6d4c4e1e84d49074ff1b27551733bc65a900aaeb7f64e60ea62df78ce6bb62c047c677b5a56e6bb8'
