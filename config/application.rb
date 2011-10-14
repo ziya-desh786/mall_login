606df319a0a6cd9beae03b173179dafee10d87e9
ZIya <ziya@My-World.(none)>
2011-10-14 15:13:05 +0530

    user add form

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
+# require "rails/test_unit/railtie"
+Bundler.require(:default, Rails.env) if defined?(Bundler)
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
